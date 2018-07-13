3029810a1ba6c57bde1716da9b17e46aa20b6898
kello22e <kello22e@mtholyoke.edu>
2018-07-13 15:06:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -17,5 +17,6 @@ class GardenGnome
     "GNARLY!!!"
   end
   def introduce_self
-    
+    "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+  end
 end
