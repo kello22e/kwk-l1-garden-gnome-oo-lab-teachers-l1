079328bff3ba47f535f05a900dc501157aaf9b9a
kello22e <kello22e@mtholyoke.edu>
2018-07-13 15:09:09 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -9,12 +9,15 @@ class GardenGnome
   def gnaw
     "Gnawing on a tree!!!"
   end
+  
   def personality
     @personality
   end
+  
   def shout
     "GNARLY!!!"
   end
+  
   def introduce_self
     "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
   end
